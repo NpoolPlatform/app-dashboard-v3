c6148b462a3d093903a1a3296ace739ac1daf382
lidaqiang <dqli161@163.com>
2023-10-13 17:33:25 +0800

    add allocated coupon column

--- src/components/inspire/AllocatedCoupon.vue
+++ src/components/inspire/AllocatedCoupon.vue
@@ -5,6 +5,7 @@
     :title='$t("MSG_ALLOCATED_COUPONS")'
     :rows='coupons'
     row-key='ID'
+    :columns='columns'
     :rows-per-page-options='[100]'
   >
     <template #top-right>
@@ -50,10 +51,15 @@
 
 <script setup lang='ts'>
 import { computed, defineAsyncComponent, onMounted, ref } from 'vue'
-import { allocatedcoupon, sdk } from 'src/npoolstore'
+import { allocatedcoupon, sdk, utils } from 'src/npoolstore'
 const CouponSelector = defineAsyncComponent(() => import('src/components/inspire/CouponSelector.vue'))
 const AppUserSelector = defineAsyncComponent(() => import('src/components/user/AppUserSelector.vue'))
 const LoadingButton = defineAsyncComponent(() => import('src/components/button/LoadingButton.vue'))
+
+import { useI18n } from 'vue-i18n'
+
+// eslint-disable-next-line @typescript-eslint/unbound-method
+const { t } = useI18n({ useScope: 'global' })
 
 const _coupon = allocatedcoupon.useAllocatedCouponStore()
 const username = ref('')
@@ -94,4 +100,108 @@ onMounted(() => {
   }
 })
 
+const columns = computed(() => [
+  {
+    name: 'ID',
+    label: t('MSG_ID'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.ID
+  },
+  {
+    name: 'AppID',
+    label: t('MSG_APP_ID'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.AppID
+  },
+  {
+    name: 'Name',
+    label: t('MSG_NAME'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.CouponName
+  },
+  {
+    name: 'Message',
+    label: t('MSG_MESSAGE'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.Message
+  },
+  {
+    name: 'Type',
+    label: t('MSG_TYPE'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.CouponType
+  },
+  {
+    name: 'Scope',
+    label: t('MSG_COUPON_SCOPE'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.CouponScope
+  },
+  {
+    name: 'Denomination',
+    label: t('MSG_DENOMINATION'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.Denomination
+  },
+  {
+    name: 'Circulation',
+    label: t('MSG_CIRCULATION'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.Circulation
+  },
+  {
+    name: 'DurationDays',
+    label: t('MSG_DURATION_DAYS'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.DurationDays
+  },
+  {
+    name: 'CouponConstraint',
+    label: t('MSG_CONSTRAINT'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.CouponConstraint
+  },
+  {
+    name: 'Random',
+    label: t('MSG_RANDOM'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.Random
+  },
+  {
+    name: 'UserID',
+    label: t('MSG_USER_ID'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.UserID
+  },
+  {
+    name: 'EmailAddress',
+    label: t('MSG_EMAIL_ADDRESS'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.EmailAddress
+  },
+  {
+    name: 'PhoneNO',
+    label: t('MSG_PHONE_NO'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.PhoneNO
+  },
+  {
+    name: 'Expired',
+    label: t('MSG_EXPIRED'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => row.Expired
+  },
+  {
+    name: 'StartAt',
+    label: t('MSG_START_AT'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => utils.formatTime(row.StartAt)
+  },
+  {
+    name: 'CreatedAt',
+    label: t('MSG_CREATED_AT'),
+    sortable: true,
+    field: (row: allocatedcoupon.Coupon) => utils.formatTime(row.CreatedAt)
+  }
+])
 </script>
