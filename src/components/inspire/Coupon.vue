92bd7a613388b9682920937722296fa889fd466a
lidaqiang <dqli161@163.com>
2023-10-16 11:24:50 +0800

    update scope

--- src/components/inspire/Coupon.vue
+++ src/components/inspire/Coupon.vue
@@ -7,63 +7,14 @@
     row-key='ID'
     :rows-per-page-options='[100]'
     :columns='columns'
-    @row-click='(evt, row, index) => onRowClick(row as coupon.Coupon)'
-  >
-    <template #top-right>
-      <div class='row indent flat'>
-        <q-btn
-          dense
-          flat
-          class='btn flat'
-          :label='$t("MSG_CREATE")'
-          @click='onCreate'
-        />
-      </div>
-    </template>
-  </q-table>
-  <q-dialog
-    v-model='showing'
-    @hide='onMenuHide'
-    position='right'
-  >
-    <q-card class='popup-menu'>
-      <q-card-section>
-        <span>{{ $t('MSG_COUPON') }}</span>
-      </q-card-section>
-      <q-card-section>
-        <!-- <AppGoodSelector v-model:id='target.AppGoodID' :label='$t("MSG_GOOD")' /> -->
-        <!-- <AppUserSelector v-model:id='target.UserID' /> -->
-        <q-input v-model='target.Name' :label='$t("MSG_NAME")' />
-        <q-input v-model='target.Message' :label='$t("MSG_MESSAGE")' />
-        <q-select :options='coupon.CouponTypes' v-model='target.CouponType' :label='$t("MSG_COUPON_TYPE")' />
-        <q-select :options='coupon.CouponConstraints' v-model='target.CouponConstraint' :label='$t("MSG_COUPON_CONSTRAINT")' />
-        <q-input v-model='target.Denomination' :label='$t("MSG_DENOMINATION")' />
-        <q-input v-model='target.Circulation' :label='$t("MSG_CIRCULATION")' />
-        <q-input v-model.number='target.DurationDays' :label='$t("MSG_DURATION_DAYS")' />
-        <q-input v-model='target.Threshold' :label='$t("MSG_THRESHOLD")' />
-        <q-select :options='coupon.CouponScopes' v-model='target.CouponScope' :label='$t("MSG_COUPON_SCOPE")' />
-      </q-card-section>
-      <q-card-section>
-        <DateTimePicker v-model:date='target.StartAt' label='MSG_START_AT' />
-      </q-card-section>
-      <q-card-section>
-        <div><q-toggle dense v-model='target.Random' :label='$t("MSG_RANDOM")' /></div>
-      </q-card-section>
-      <q-item class='row'>
-        <LoadingButton loading :label='$t("MSG_SUBMIT")' @click='onSubmit' />
-        <q-btn class='btn round' :label='$t("MSG_CANCEL")' @click='onCancel' />
-      </q-item>
-    </q-card>
-  </q-dialog>
+  />
   <CouponScope />
   <AllocatedCoupon />
 </template>
 
 <script setup lang='ts'>
-import { computed, defineAsyncComponent, onMounted, ref } from 'vue'
+import { computed, defineAsyncComponent, onMounted } from 'vue'
 import { coupon, sdk, utils } from 'src/npoolstore'
-const DateTimePicker = defineAsyncComponent(() => import('src/components/date/DateTimePicker.vue'))
-const LoadingButton = defineAsyncComponent(() => import('src/components/button/LoadingButton.vue'))
 const AllocatedCoupon = defineAsyncComponent(() => import('src/components/inspire/AllocatedCoupon.vue'))
 const CouponScope = defineAsyncComponent(() => import('src/components/inspire/CouponScope.vue'))
 
@@ -74,48 +25,6 @@ const { t } = useI18n({ useScope: 'global' })
 
 const _coupon = coupon.useCouponStore()
 const coupons = computed(() => _coupon.coupons())
-
-const target = ref({} as coupon.Coupon)
-const showing = ref(false)
-const updating = ref(false)
-
-const onCreate = () => {
-  target.value = {} as coupon.Coupon
-  showing.value = true
-  updating.value = false
-}
-const onRowClick = (row: coupon.Coupon) => {
-  target.value = { ...row }
-  showing.value = true
-  updating.value = true
-}
-const onMenuHide = () => {
-  showing.value = false
-  target.value = {} as coupon.Coupon
-}
-const onCancel = () => {
-  onMenuHide()
-}
-
-const onSubmit = (done: () => void) => {
-  if (updating.value) {
-    sdk.updateCoupon(target.value, (error: boolean) => {
-      done()
-      if (error) {
-        return
-      }
-      onMenuHide()
-    })
-    return
-  }
-  sdk.createCoupon(target.value, (error: boolean) => {
-    done()
-    if (error) {
-      return
-    }
-    onMenuHide()
-  })
-}
 
 onMounted(() => {
   if (!coupons.value?.length) {
