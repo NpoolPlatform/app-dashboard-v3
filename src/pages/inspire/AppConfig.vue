4a7f7550d7e5cbf8ecd2a8f9c7368bec997ec94f
jakys <jakysing@163.com>
2024-04-07 09:30:17 +0800

    Add inspire commission config

---
+++ src/pages/inspire/AppConfig.vue
@@ -0,0 +1,13 @@
+<template>
+  <AppConfig />
+</template>
+
+<script setup lang='ts'>
+import { defineAsyncComponent } from 'vue'
+
+const AppConfig = defineAsyncComponent(() => import('src/components/inspire/AppConfig.vue'))
+
+</script>
+
+<style lang='sass' scoped>
+</style>
