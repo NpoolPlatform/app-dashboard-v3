8d9f0a21fa65bad1b4090b0a2729cc73a6e665ff
lidaqiang <dqli161@163.com>
2024-07-02 15:33:56 +0800

    support update appfee

--- src/components/good/AppFee.vue
+++ src/components/good/AppFee.vue
@@ -7,18 +7,74 @@
     :columns='appFeesColumns'
     row-key='ID'
     :rows-per-page-options='[100]'
+    @row-click='(evt, row, index) => onRowClick(row as appfee.AppFee)'
   />
+  <q-dialog
+    v-model='showing'
+    @hide='onMenuHide'
+    position='right'
+  >
+    <q-card class='popup-menu'>
+      <q-card-section>
+        <span>{{ $t('MSG_APP_DEFAULT_GOOD') }}</span>
+      </q-card-section>
+      <q-card-section>
+        <q-input v-model='target.AppGoodName' :label='$t("MSG_NAME")' />
+        <q-input v-model='target.Banner' :label='$t("MSG_BANNER")' />
+        <q-input v-model='target.ProductPage' :label='$t("MSG_PRODUCT_PAGE")' />
+        <q-input v-model.number='target.UnitValue' :label='$t("MSG_UNIT_VALUE")' />
+        <q-input v-model.number='target.MinOrderDurationSeconds' :label='$t("MSG_MIN_ORDER_DURATION_SECONDS")' />
+        <q-select :options='goodbase.CancelModes' v-model='target.CancelMode' :label='$t("MSG_CANCEL_MODE")' />
+      </q-card-section>
+      <q-item class='row'>
+        <q-btn class='btn round' :loading='submitting' :label='$t("MSG_SUBMIT")' @click='onSubmit' />
+        <q-btn class='btn round' :label='$t("MSG_CANCEL")' @click='onCancel' />
+      </q-item>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script setup lang='ts'>
-import { sdk, utils, appfee } from 'src/npoolstore'
-import { computed, onMounted } from 'vue'
+import { sdk, utils, appfee, goodbase } from 'src/npoolstore'
+import { computed, onMounted, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t } = useI18n({ useScope: 'global' })
 
 const appFees = computed(() => sdk.appFees.value)
+const target = ref({} as appfee.AppFee)
+
+const showing = ref(false)
+const updating = ref(false)
+const submitting = ref(false)
+
+const onMenuHide = () => {
+  target.value = {} as appfee.AppFee
+  showing.value = false
+  submitting.value = false
+}
+
+const onCancel = () => {
+  onMenuHide()
+}
+
+const onRowClick = (row: appfee.AppFee) => {
+  target.value = { ...row }
+  updating.value = true
+  showing.value = true
+}
+
+const onSubmit = () => {
+  submitting.value = true
+  sdk.updateAppFee(target.value, (error: boolean) => {
+    submitting.value = false
+    if (error) {
+      return
+    }
+    onMenuHide()
+  })
+}
 
 const appFeesColumns = computed(() => [
   {
