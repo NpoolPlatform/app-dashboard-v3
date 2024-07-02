76de316649b3d398ed214f9a179c1f9c70741e32
lidaqiang <dqli161@163.com>
2024-07-02 19:23:53 +0800

    add malfunctions

--- src/components/good/AppPowerRental.vue
+++ src/components/good/AppPowerRental.vue
@@ -3,6 +3,7 @@
     :title='"AppGoods"'
   />
   <AppFee />
+  <MalFunction />
   <AppDefaultGood />
 </template>
 <script setup lang='ts'>
@@ -11,5 +12,6 @@ import { defineAsyncComponent } from 'vue'
 const GoodBase = defineAsyncComponent(() => import('src/components/good/GoodBase.vue'))
 const AppFee = defineAsyncComponent(() => import('src/components/good/AppFee.vue'))
 const AppDefaultGood = defineAsyncComponent(() => import('src/components/good/AppDefaultGood.vue'))
+const MalFunction = defineAsyncComponent(() => import('src/components/good/MalFunction.vue'))
 
 </script>
---
+++ src/components/good/MalFunction.vue
@@ -0,0 +1,103 @@
+<template>
+  <q-table
+    dense
+    flat
+    :title='$t("MSG_MALFUNCTIONS")'
+    :rows='malfunctions'
+    :columns='malfunctionColumns'
+    row-key='ID'
+    :rows-per-page-options='[100]'
+  />
+</template>
+
+<script setup lang='ts'>
+import { sdk, utils, goodmalfunction } from 'src/npoolstore'
+import { computed, onMounted } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+// eslint-disable-next-line @typescript-eslint/unbound-method
+const { t } = useI18n({ useScope: 'global' })
+
+const malfunctions = computed(() => sdk.goodMalfunctions.value)
+
+const malfunctionColumns = computed(() => [
+  {
+    name: 'ID',
+    label: t('MSG_ID'),
+    sortable: true,
+    field: (row: goodmalfunction.Malfunction) => row.ID
+  },
+  {
+    name: 'EntID',
+    label: t('MSG_ENT_ID'),
+    sortable: true,
+    field: (row: goodmalfunction.Malfunction) => row.EntID
+  },
+  {
+    name: 'GOODID',
+    label: t('MSG_GOODID'),
+    sortable: true,
+    field: (row: goodmalfunction.Malfunction) => row.GoodID
+  },
+  {
+    name: 'GOODNAME',
+    label: t('MSG_GOODNAME'),
+    sortable: true,
+    field: (row: goodmalfunction.Malfunction) => row.GoodName
+  },
+  {
+    name: 'Title',
+    label: t('MSG_TITLE'),
+    sortable: true,
+    field: (row: goodmalfunction.Malfunction) => row.Title
+  },
+  {
+    name: 'Message',
+    label: t('MSG_MESSAGE'),
+    sortable: true,
+    field: (row: goodmalfunction.Malfunction) => row.Message
+  },
+  {
+    name: 'GOODTYPE',
+    label: t('MSG_GOOD_TYPE'),
+    sortable: true,
+    field: (row: goodmalfunction.Malfunction) => row.GoodType
+  },
+  {
+    name: 'StartAt',
+    label: t('MSG_START_AT'),
+    sortable: true,
+    field: (row: goodmalfunction.Malfunction) => utils.formatTime(row.StartAt)
+  },
+  {
+    name: 'DurationSeconds',
+    label: t('MSG_DURATION_SECONDS'),
+    sortable: true,
+    field: (row: goodmalfunction.Malfunction) => row.DurationSeconds
+  },
+  {
+    name: 'CompensateSeconds',
+    label: t('MSG_COMPENSATE_SECONDS'),
+    sortable: true,
+    field: (row: goodmalfunction.Malfunction) => row.CompensateSeconds
+  },
+  {
+    name: 'CompensateOrders',
+    label: t('MSG_COMPENSATE_ORDERS'),
+    sortable: true,
+    field: (row: goodmalfunction.Malfunction) => row.CompensateOrders
+  },
+  {
+    name: 'CreatedAt',
+    label: t('MSG_CREATED_AT'),
+    sortable: true,
+    field: (row: goodmalfunction.Malfunction) => utils.formatTime(row?.CreatedAt)
+  }
+])
+
+onMounted(() => {
+  if (!malfunctions.value?.length) {
+    sdk.getMalfunctions(0, 0)
+  }
+})
+</script>
