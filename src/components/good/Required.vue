1645f3000b397db74fd557ea93fb02a2bc7f7f83
lidaqiang <dqli161@163.com>
2024-07-08 19:25:42 +0800

    add RequiredGood

---
+++ src/components/good/Required.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-table
+    dense
+    flat
+    :title='$t("MSG_REQUIRED_GOODS")'
+    :rows='requireds'
+    row-key='ID'
+    :rows-per-page-options='[100]'
+  />
+</template>
+
+<script setup lang='ts'>
+import { onMounted } from 'vue'
+import { sdk } from 'src/npoolstore'
+
+const requireds = sdk.requiredGoods
+
+onMounted(() => {
+  if (!requireds.value.length) {
+    sdk.getRequiredGoods(0, 0)
+  }
+})
+
+</script>
+
+<style lang='sass' scoped>
+</style>
