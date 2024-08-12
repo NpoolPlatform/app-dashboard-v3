715079f4bc5afc2027d8ed8d55681375bda04028
lidaqiang <dqli161@163.com>
2024-08-12 18:35:16 +0800

    fix target

--- quasar.conf.js
+++ quasar.conf.js
@@ -88,8 +88,7 @@ module.exports = configure(function (ctx) {
       proxy: {
         '/api': {
           // target: 'https://api.procyon.vip',
-          // target: 'https://api.npool.top:20402',
-          target: 'https://api.procyon.xpool.top',
+          target: 'https://api.npool.top:20402',
           pathRewrite: {
             '^/api': '/api'
           },
